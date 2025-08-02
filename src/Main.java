bd7fa944ae3af3880d6f508a0ab3a3488fb048c2
EzekielRangel <ezekiel-6540128@educar.rs.gov.br>
2025-08-02 00:53:04 -0300

    Criação de cadastros

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner reader = new Scanner(System.in);
+        perguntas listaPrincipal = new perguntas();
+        String nomePF = "";
+        String dataNascimento1 = null;
+        long cpfCadastro1 = -1;
+        int option = -1;
+
+        while(option != 3){
+            System.out.println(listaPrincipal.listaPerguntas);
+            option = reader.nextInt();
+            if (!Arrays.asList(perguntas.quantidadeDePerguntas).contains(option)){
+                System.out.println("Opção digitada inválida, por favor digite uma opção válida");
+            }
+
+            switch(option){
+                case 1:
+                    System.out.println(listaPrincipal.caso1Perguntas);
+                    reader.nextLine();
+                    System.out.println("Digite o nome: ");
+                    nomePF = reader.nextLine();
+                    System.out.println("Digite o CPF: ");
+                    cpfCadastro1 = reader.nextLong();
+                    System.out.println("Digite a data de nascimento (dd/mm/aaaa)");
+                    dataNascimento1 = reader.nextLine();
+                    reader.nextLine();
+                    break;
+                case 2:
+                    System.out.println("Digite o CPF de quem gostaria de consultar:  \n");
+                    long cpfDigitadoConsulta = reader.nextLong();
+                    if(cpfCadastro1 == -1){
+                        System.out.println("Nenhum usuário cadastrado ainda");
+                    } else if(cpfDigitadoConsulta == cpfCadastro1){
+                        System.out.printf("Nome %S \n" +
+                                "CPF %d \n" +
+                                "DATA DE NASCIMENTO %s \n", nomePF, cpfCadastro1, dataNascimento1);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Programa finalizado com sucesso");
+                    break;
+            }
+        }
     }
 }
---
+++ src/perguntas.java
@@ -0,0 +1,21 @@
+public class perguntas {
+    public static int[] quantidadeDePerguntas = {1, 2, 3};
+
+    String listaPerguntas = ("=====Digite o que deseja realizar=====\n" +
+            "1 - Cadastrar usuário\n" +
+            "2 - Consultar usuário\n" +
+            "3 - Sair do programa");
+
+
+    String caso1Perguntas = ("Digite as informações do usuario que gostaria de cadastrar\n" +
+       //botar a variavel aqui do main, pra ja ir faznedo a alteração e salvando o valor do cpf
+            "INFORMAÇÕES NECESSÁRIAS\n");
+
+
+}
+
+
+
+
+
+
